4d7d286b7a8fe35a57511d9656d3038d75fc8cff
Michał Chojnacki <m.chojnacki@muyo.io>
2015-08-21 18:21:19 +0200

    [Utils] Added a base PHP introspection utility.

---
+++ Php.php
@@ -0,0 +1,79 @@
+<?php namespace nyx\utils;
+
+// External dependencies
+use nyx\core;
+
+/**
+ * PHP
+ *
+ * Utils for introspecting information about PHP itself.
+ *
+ * @package     Nyx\Utils\System
+ * @version     0.0.1
+ * @author      Michal Chojnacki <m.chojnacki@muyo.io>
+ * @copyright   2012-2016 Nyx Dev Team
+ * @link        http://docs.muyo.io/nyx/utils/system.html
+ * @todo        Bitwise checks for combined phpinfo() sections when parsing information.
+ */
+class Php
+{
+    /**
+     * The traits of the Php class.
+     */
+    use traits\StaticallyExtendable;
+
+    /**
+     * @var bool    Whether PHP has been compiled with the "--enable-sigchild" flag.
+     */
+    private static $flags = [];
+
+    /**
+     * @var array   An array containing the output of phpinfo() calls, grouped together by the INFO_ constants
+     *              used to retrieve them.
+     */
+    private static $info = [];
+
+    /**
+     * Checks whether PHP has been compiled with the given flag.
+     *
+     * @param   string  $flag   The name of the flag to check. The two initial hyphens can be omitted.
+     * @return  bool            True when PHP has been compiled with the given flag, false otherwise.
+     */
+    public static function hasFlag($flag)
+    {
+        // Standardize the flag name - remove starting hyphens.
+        $flag = ltrim((string) $flag, '-');
+
+        // Return the check right away if it's already cached.
+        if (isset(static::$flags[$flag])) {
+            return static::$flags[$flag];
+        }
+
+        // Grab the output of phpinfo(). If INFO_ALL is already available, we will just parse it instead of
+        // fetching INFO_GENERAL specifically for our case. Then we are simply going to check if the --string
+        // appears in the info.
+        $result = strpos(isset(static::$info[INFO_ALL]) ? static::$info[INFO_ALL] : static::getInfo(INFO_GENERAL), '--'.$flag);
+
+        // Cache the result and return it.
+        return static::$flags[$flag] = false !== $result;
+    }
+
+    /**
+     * Fetches, caches and returns the output of phpinfo().
+     *
+     * @param   int     $for    One of the INFO_* constants {@see http://php.net/manual/en/function.phpinfo.php}.
+     * @return  string          The raw output of phpinfo.
+     */
+    public static function getInfo($for = INFO_ALL)
+    {
+        // Return the data right away if we've already cached the given section.
+        if (isset(static::$info[$for])) {
+            return static::$info[$for];
+        }
+
+        ob_start();
+        phpinfo($for);
+
+        return static::$info[$for] = ob_get_clean();
+    }
+}
