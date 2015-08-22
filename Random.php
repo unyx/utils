0b879ceaa70f09403b5230867d78740fc8bdfb08
Michał Chojnacki <m.chojnacki@muyo.io>
2015-08-22 09:12:44 +0200

    [Utils] Random: assert valid min/max ranges and handle other unexpected cases with Exceptions since the engine fails silently (until/unless https://wiki.php.net/rfc/core-function-exceptions passes)

--- Random.php
+++ Random.php
@@ -29,12 +29,17 @@ class Random
      *       we bump this up to an Exception to be on the safe side.
      *       Please {@see http://php.net/manual/en/function.random-bytes.php} for when this may be the case.
      *
-     * @param   int     $length     The length of the random string of bytes that should be generated.
-     * @return  string              The resulting string in binary format.
-     * @throws  \RuntimeException   When the platform specific RNG couldn't be used for some reason.
+     * @param   int     $length             The length of the random string of bytes that should be generated.
+     * @return  string                      The resulting string in binary format.
+     * @throws  \InvalidArgumentException   When a expected length smaller than 1 was given.
+     * @throws  \RuntimeException           When the platform specific RNG couldn't be used for some reason.
      */
     public static function bytes(int $length) : string
     {
+        if ($length < 1) {
+            throw new \InvalidArgumentException('The expected amount of random bytes must be at least 1.');
+        }
+
         if (false === $result = random_bytes($length)) {
             throw new \RuntimeException('No sufficient source of randomness is available on this platform.');
         }
@@ -50,13 +55,18 @@ class Random
      *       we bump this up to an Exception to be on the safe side.
      *       Please {@see http://php.net/manual/en/function.random-int.php} for when this may be the case.
      *
-     * @param   int     $min        The minimal value of the generated integer. Must be >= than PHP_INT_MIN.
-     * @param   int     $max        The maximal value of the generated integer. Must be <= than PHP_INT_MAX.
-     * @return  int                 The generated integer.
-     * @throws  \RuntimeException   When the platform specific RNG couldn't be used for some reason.
+     * @param   int     $min                The minimal expected value of the generated integer (>= than PHP_INT_MIN).
+     * @param   int     $max                The maximal expected value of the generated integer (<= than PHP_INT_MAX).
+     * @return  int                         The generated integer.
+     * @throws  \InvalidArgumentException   When the minimal expected value is bigger than the maximal expected value.
+     * @throws  \RuntimeException           When the platform specific RNG couldn't be used for some reason.
      */
     public static function int(int $min = 0, int $max = PHP_INT_MAX) : int
     {
+        if ($min > $max) {
+            throw new \DomainException('The minimal expected value must be smaller than the maximal expected value.');
+        }
+
         if (false === $result = random_int($min, $max)) {
             throw new \RuntimeException('No sufficient source of randomness is available on this platform.');
         }
@@ -70,9 +80,14 @@ class Random
      * @param   int     $min        The minimal value of the generated float. Must be >= than PHP_INT_MIN.
      * @param   int     $max        The maximal value of the generated float. Must be <= than PHP_INT_MAX.
      * @return  float               The generated float.
+     * @throws  \InvalidArgumentException   When the minimal expected value is bigger than the maximal expected value.
      */
     public static function float(int $min = 0, int $max = 1) : float
     {
+        if ($min > $max) {
+            throw new \DomainException('The minimal expected value must be smaller than the maximal expected value.');
+        }
+
         return $min + static::int() / PHP_INT_MAX * ($max - $min);
     }
 
@@ -80,19 +95,24 @@ class Random
      * Generates a pseudo-random string of the specified length using random alpha-numeric (base64)
      * characters or the characters provided.
      *
+     * Triggers an E_USER_NOTICE error if a $characters list containing only one character is given
+     * while at the same time expecting a generated string with a $length > 1, since this results
+     * in repeating that character $length number of times and is a dangerous op in a cryptographic
+     * context.
+     *
      * Aliases:
      *  - @see Str::random()
      *
-     * @param   int     $length     The expected length of the generated string.
-     * @param   string  $characters The character list to use. If not given, the method will fall back
-     *                              to the Base64 character set.
-     * @return  float               The generated string.
-     * @throws  \DomainException    When a expected length smaller than 1 was given.
+     * @param   int     $length             The expected length of the generated string.
+     * @param   string  $characters         The character list to use. If not given, the method will
+     *                                      fall back to the Base64 character set.
+     * @return  float                       The generated string.
+     * @throws  \InvalidArgumentException   When a expected length smaller than 1 was given.
      */
     public static function string(int $length = 8, string $characters = null) : string
     {
         if ($length < 1) {
-            throw new \DomainException('The expected length of the generated string must be at least 1.');
+            throw new \InvalidArgumentException('The expected length of the generated string must be at least 1.');
         }
 
         // Expected most common use case is using the base64 character set, ie. when no
@@ -103,9 +123,19 @@ class Random
             return substr(rtrim(base64_encode($bytes), '='), 0, $length);
         }
 
-        // If only one character was given, we're gonna repeat it $length times
-        // in a *totally random* order, d'oh.
+        // If only a single character was given...
         if (1 === $charactersLen = strlen($characters)) {
+
+            // ... and we only expected one to be generated, d'oh, we're gonna return it.
+            if ($charactersLen === $length) {
+                return $characters;
+            }
+
+            // Since this might be done in a cryptographic context, at least be sassy about it
+            // and notify the user that we do not find this amusing.
+            trigger_error('Attempted to generate a random string of '.$length.' characters but was given only 1 character to create it out of. This is potentially unsafe.');
+
+            // We're gonna repeat it $length times in a *totally random* order, d'oh.
             return str_repeat($charactersLen, $length);
         }
 
