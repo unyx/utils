e5f92f797e60e438041b5fd391bef75cafe902ce
Michał Chojnacki <m.chojnacki@muyo.io>
2015-08-27 08:17:36 +0200

    [Utils/Str] Started abstracing away character-specific functionality into a Character class.

--- Str.php
+++ Str.php
@@ -128,28 +128,6 @@ class Str
     }
 
     /**
-     * Returns the character at the specified $offset (0-indexed) in $haystack.
-     *
-     * @param   string      $haystack   The string to search in.
-     * @param   int         $offset     The requested index. If a negative index is given, this method will return
-     *                                  the $offset-th character counting from the end of the string.
-     * @param   string|null $encoding   The encoding to use.
-     * @return  string                  The character at the specified $index.
-     */
-    public static function at(string $haystack, int $offset, string $encoding = null) : string
-    {
-        $encoding = $encoding ?: static::encoding($haystack);
-
-        // Check if the absolute starting index (to account for negative indexes) + 1 (since it's 0-indexed
-        // while length is > 1 at this point) is within the length of the string.
-        if (abs($offset) >= mb_strlen($haystack, $encoding)) {
-            throw new \OutOfBoundsException('The given $offset ['.$offset.'] does not exist within the string ['.static::truncate($haystack, 20, '...', $encoding).'].');
-        }
-
-        return mb_substr($haystack, $offset, 1, $encoding);
-    }
-
-    /**
      * Finds the first occurrence of $needle within $haystack and returns the part of $haystack
      * *before* the $needle.
      *
