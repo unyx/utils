df79731ec6053e5117c259d8b2a9a9174a26dc24
Michał Chojnacki <m.chojnacki@muyo.io>
2015-08-27 17:05:51 +0200

    [Utils/Math] Added DivisionByZero Exception.

---
+++ math/exceptions/DivisionByZero.php
@@ -0,0 +1,15 @@
+<?php namespace nyx\utils\math\exceptions;
+
+/**
+ * Division by Zero Exception
+ *
+ * @package     Nyx\Utils\Math
+ * @version     0.1.0
+ * @author      Michal Chojnacki <m.chojnacki@muyo.io>
+ * @copyright   2012-2016 Nyx Dev Team
+ * @link        http://docs.muyo.io/nyx/utils/math.html
+ */
+class DivisionByZero extends \RuntimeException
+{
+
+}
