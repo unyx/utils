f6e3f7c1d1c0295a47488c63373b85558456528c
Michał Chojnacki <m.chojnacki@muyo.io>
2015-08-29 07:14:21 +0200

    [Utils/Math] Added placeholder Vector2D and VectorND classes to start optimizing them where it's possible with not overly non-DRY code.

---
+++ math/vectors/Vector2D.php
@@ -0,0 +1,18 @@
+<?php namespace nyx\utils\math\vectors;
+
+// Internal dependencies
+use nyx\utils\math;
+
+/**
+ * 2-dimensional Vector
+ *
+ * @package     Nyx\Utils\Math
+ * @version     0.1.0
+ * @author      Michal Chojnacki <m.chojnacki@muyo.io>
+ * @copyright   2012-2016 Nyx Dev Team
+ * @link        http://docs.muyo.io/nyx/utils/math.html
+ */
+class Vector2D extends math\Vector
+{
+
+}
