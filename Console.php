ef1c9ace5e7b2de2d1dcd8e621dd38380a0c22fb
Michał Chojnacki <m.chojnacki@muyo.io>
2016-04-05 13:33:27 +0200

    [Utils/Console] Docblock/CS

--- Console.php
+++ Console.php
@@ -7,36 +7,37 @@
  * @version     0.0.1
  * @author      Michal Chojnacki <m.chojnacki@muyo.io>
  * @copyright   2012-2016 Nyx Dev Team
- * @link        http://docs.muyo.io/nyx/utils/files.html
+ * @link        http://docs.muyo.io/nyx/utils/console.html
  */
 class Console
 {
     /**
      * Escapes a (potentially unsafe) string to be used as a shell argument.
      *
-     * @param   string  $string     The argument to escape.
+     * @param   string  $argument   The argument to escape.
      * @return  string              The sanitized string.
      */
     public static function escape(string $argument) : string
     {
-        // Fix for PHP bug #49446 {@see https://bugs.php.net/bug.php?id=49446}
-        // Fix for PHP bug #43784 {@see https://bugs.php.net/bug.php?id=43784}
-        if (Platform::isWindows()) {
-            $result = '';
-
-            foreach (preg_split('/([%"])/i', $argument, -1, PREG_SPLIT_NO_EMPTY | PREG_SPLIT_DELIM_CAPTURE) as $part) {
-                if ('"' == $part) {
-                    $result .= '\\"';
-                } elseif ('%' == $part) {
-                    $result .= '^%';
-                } else {
-                    $result .= escapeshellarg($part);
-                }
-            }
-
-            return $result;
+        if (!Platform::isWindows()) {
+            return preg_match('{^[\w-]+$}', $argument) ? $argument : escapeshellarg($argument);
         }
 
-        return preg_match('{^[\w-]+$}', $argument) ? $argument : escapeshellarg($argument);
+        // Windows
+        // Fix for PHP bug #49446 {@see https://bugs.php.net/bug.php?id=49446}
+        // Fix for PHP bug #43784 {@see https://bugs.php.net/bug.php?id=43784}
+        $result = '';
+
+        foreach (preg_split('/([%"])/i', $argument, -1, PREG_SPLIT_NO_EMPTY | PREG_SPLIT_DELIM_CAPTURE) as $part) {
+            if ('"' == $part) {
+                $result .= '\\"';
+            } elseif ('%' == $part) {
+                $result .= '^%';
+            } else {
+                $result .= escapeshellarg($part);
+            }
+        }
+
+        return $result;
     }
 }
