cddfa5f07814b4b861f356eafa7fe470b9100602
Michał Chojnacki <m.chojnacki@muyo.io>
2016-04-07 22:49:51 +0200

    [Utils/Str] Starting work on the Cases utility.

---
+++ str/Cases.php
@@ -0,0 +1,23 @@
+<?php namespace nyx\utils\str;
+
+// Internal includes
+use nyx\utils;
+
+/**
+ * Cases
+ *
+ * Multi-byte safe utility for manipulating the character case in strings.
+ *
+ * @package     Nyx\Utils\Strings
+ * @version     0.1.0
+ * @author      Michal Chojnacki <m.chojnacki@muyo.io>
+ * @copyright   2012-2016 Nyx Dev Team
+ * @link        http://docs.muyo.io/nyx/utils/strings.html
+ */
+class Cases
+{
+    /**
+     * The traits of the Cases class.
+     */
+    use utils\traits\StaticallyExtendable;
+}
